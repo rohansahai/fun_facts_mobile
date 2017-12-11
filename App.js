47443dc9cd4879e59d272a43ae630cc09a90941f
Rohan Sahai <rohan@affinity.co>
2017-12-10 23:22:55 -0800

    some breaking up of code and stuff

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Swiper from 'react-native-swiper';
+import Fetch from 'whatwg-fetch';
 
 var styles = StyleSheet.create({
   wrapper: {
@@ -31,19 +32,51 @@ var styles = StyleSheet.create({
 })
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {slidesData: []}
+  }
+
+  componentDidMount() {
+    let factsEndpoint = "https://thawing-escarpment-65092.herokuapp.com/facts/"
+    fetch(factsEndpoint).then(response => {
+      return response.json()
+    }).then(json => {
+      this.setState({slidesData: json})
+      console.log('parsed json', json)
+    }).catch(ex => {
+      console.log('parsing failed', ex)
+    })
+
+    let slidesData = [
+      {id: 1, text: 'Swipe left to find out if you are smelly'},
+      {id: 2, text: 'you are an angel who smells like cherry blossoms'},
+      {id: 3, text: 'mmmmmmmmm spaghett!'},
+    ]
+
+    this.setState({slidesData: slidesData})
+  }
   render() {
+    let slides = this.state.slidesData.map(
+      slide => (
+        <Slide text={slide.text} key = {slide.id}/>
+      )
+    )
+
     return (
       <Swiper style={styles.wrapper} showsButtons={true}>
-        <View style={styles.slide1}>
-          <Text style={styles.text}>Hello Swiper</Text>
-        </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
-        </View>
+        {slides}
       </Swiper>
+    )
+  }
+}
+
+class Slide extends React.Component {
+  render() {
+    return (
+      <View style={styles.slide1}>
+        <Text style={styles.text}>{this.props.text}</Text>
+      </View>
     )
   }
 }
